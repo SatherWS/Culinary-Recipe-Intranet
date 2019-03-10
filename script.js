43e19c69f961b201a96fed9c81ab269fee18ff40
Fendricks <35883253+Fendricks@users.noreply.github.com>
2019-03-10 14:22:06 -0400

    Bug: 1st slide takes two clicks for display: none

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-// Testimonial Slide show    
-/*var slideIndex = 1;
-    showSlides(slideIndex);
-    
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}    
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    
-// If Else Statements
-if (n > slides.length){
-    slideIndex=1;
-}
-if (n < 1) {
-    slideIndex = slides.lenght;
-}
-    for (i=0; i < dots.length; i++){
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " active";
-}*/
+// Slide Show script
 var slideIndex = 0;
 showDivs(slideIndex);
 
